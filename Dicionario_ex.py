fe04c86f6fbb174836b5fe72baabea7176478663
Pedro Henrique do Prado <104167056+PHPPrado@users.noreply.github.com>
2022-08-16 15:35:01 -0300

    Add files via upload

---
+++ Dicionario_ex.py
@@ -0,0 +1,14 @@
+usuarios = {}
+print(usuarios)
+
+usuarios = {
+    "chaves" : ["Chaves do 8", "24/12/2017", "Recep_01"],
+    "quico" : ["Quico das Flores", "20/12/2017", "Raiox_03"]
+}
+print(usuarios)
+
+usuarios ["florinda"] = ["Dona Florinda", "24/12/2017", "Raiox_01"]
+print(usuarios)
+
+print(usuarios.get("quico"))
+
